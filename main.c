13af88d2944671a80cb55265f1359ff65f7e79f0
Michael Koval <koval.michael@gmail.com>
2010-08-23 00:50:21 -0400

    Replaced the test code in main() with a simple command-line interface that will be used for training.

--- main.c
+++ main.c
@@ -1,21 +1,53 @@
 #include <stdio.h>
+#include <string.h>
 #include <util/delay.h>
 #include "libarduino2.h"
+#include "sensor.h"
+
+typedef enum {
+    MODE_MENU,
+    MODE_FOLLOW,
+    MODE_TRAIN
+} mode_t;
 
 int main(void) {
-    analog_init();
+    mode_t mode = MODE_MENU;
+    char cmd[16];
+
+    sensor_init();
     serial_init();
 
-    for (;;) {
-        if (analog_available()) {
-            printf("%4d %4d %4d %4d\n",
-                analog_get(0),
-                analog_get(1),
-                analog_get(2),
-                analog_get(3)
-            );
-        } else {
-            printf("wait\n");
+    for (;;)
+    switch (mode) {
+    case MODE_MENU:
+        /* Prompt the user for a command. */
+        printf(" >>> ");
+        readline(cmd, cmd + sizeof(cmd));
+
+        /* Calibrate the line following sensors. */
+        if (!strcmp(cmd, "line")) {
+            sensor_line();
+            printf("Line calibration complete.\n");
+        } else if (!strcmp(cmd, "floor")) {
+            sensor_floor();
+            printf("Floor calibration complete.\n");
         }
+        /* Follow the line with a machine learning algorithm. */
+        else if (!strcmp(cmd, "follow")) {
+            mode = MODE_FOLLOW;
+        } else if (!strcmp(cmd, "train")) {
+            mode = MODE_TRAIN;
+        }
+        /* Unrecognized command. */
+        else {
+            printf("Unrecognized command.\n");
+        }
+        break;
+
+    case MODE_FOLLOW:
+        break;
+
+    case MODE_TRAIN:
+        break;
     }
 }
