b50a853d137dfa0b3171d101eb08c9ccc9320a21
Michael Koval <koval.michael@gmail.com>
2010-08-24 14:37:54 -0400

    Changed sensor_t so its passing semantics are the same as motor_t.

--- sensor.c
+++ sensor.c
@@ -46,17 +46,17 @@ void sensor_line(sensor_config_t *config) {
     }
 }
 
-void sensor_update(sensor_config_t const *config, sensor_t sen) {
+void sensor_update(sensor_config_t const *config, sensor_t *sen) {
     uint8_t i;
     for (i = 0; i < SENSOR_NUM; ++i) {
         float value = analog_get(i);
 
         /* Force the line to produce higher sensor readings than the floor. */
         if (config->line[i] > config->floor[i]) {
-            sen[i] = (value - config->floor[i]) / config->line[i];
+            sen->value[i] = (value - config->floor[i]) / config->line[i];
         } else {
-            sen[i] = 1.0f - (value - config->line[i]) / config->floor[i];
+            sen->value[i] = 1.0f - (value - config->line[i]) / config->floor[i];
         }
-        sen[i] = CONSTRAIN(sen[i], 0.0f, 1.0f);
+        sen->value[i] = CONSTRAIN(sen->value[i], 0.0f, 1.0f);
     }
 }
--- sensor.h
+++ sensor.h
@@ -18,11 +18,14 @@ typedef struct {
     uint16_t floor[SENSOR_NUM];
     uint16_t line[SENSOR_NUM];
 } sensor_config_t;
-typedef float sensor_t[4];
+
+typedef struct {
+    float value[SENSOR_NUM];
+} sensor_t;
 
 void sensor_init(sensor_config_t *);
 void sensor_line(sensor_config_t *);
 void sensor_floor(sensor_config_t *);
-void sensor_update(sensor_config_t const *, sensor_t);
+void sensor_update(sensor_config_t const *, sensor_t *);
 
 #endif
